7a4160f5f129f3b770185b609335740de5e18084
Maximiliano Montaña <maxi.omr01@gmail.com>
2024-11-27 18:30:30 -0300

    add whatsapp icon to main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,8 +11,48 @@ import Works from "./ui/home/works/works";
 export default function Home() {
   return (
     <>
-      <BtnWhatsapp />
       <BtnInstagram />
+      {/* <div
+        className="fixed z-50 bottom-0 right-0 w-1/2 flex justify-end border-l
+lg:z-[80] lg:bottom-16 lg:right-10 lg:w-auto lg:border-none"
+      >
+        <Link
+          legacyBehavior
+          href={`https://wa.me/5556666666?text=${encodeURIComponent(
+            "ffffffffffff"
+          )}`}
+          passHref
+        >
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            className="bg-gray-500 px-5 py-2 flex flex-col w-full h-20 justify-center
+lg:bg-green-400 lg:rounded-2xl lg:flex-row lg:gap-3 lg:w-auto lg:h-auto lg:hover:bg-green-600 lg:transition lg:duration-300"
+          >
+            {" "}
+            <div
+              className="text-white hidden
+lg:block"
+            >
+              Hablame
+            </div>
+            <div className="w-full flex justify-center">
+              <FaWhatsapp
+                className="text-white text-3xl
+lg:text-2xl"
+              />
+            </div>
+            <span
+              className="block text-white text-center w-full
+lg:hidden"
+            >
+              WhatsApp
+            </span>
+          </a>
+        </Link>
+      </div> */}
+      <BtnWhatsapp />
+
       <Cover />
       <About />
       <Projects />
--- src/app/ui/home/cover/cover.tsx
+++ src/app/ui/home/cover/cover.tsx
@@ -8,7 +8,14 @@ const Cover = () => {
   return (
     <div className={styles.container}>
       <div className={styles.mainImgContainer}>
-        <Image src={mainCover} alt="civiltec" layout="fill" objectFit="cover" />
+        <Image src={mainCover} alt="civiltec" 
+        //layout="fill" objectFit="cover" 
+        fill
+        // style={{ objectFit: "cover" }}
+        //objectFit="cover"
+        // layout="fill"
+        sizes="(max-width: 768px) 100vw, (max-width: 1200px) 100vw, 100vw"
+        className={`${styles.imgZoomEffect} object-[37%_100%] md:object-center   object-cover`} />
       </div>
     </div>
   );
